d8390b491579bb5313b056931c247ea2e1018448
Palem Ravi chandra <123462175+Palemravichandra@users.noreply.github.com>
2023-06-03 21:57:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,44 +9,60 @@ from xgboost import XGBRegressor
 pipe = pickle.load(open('lap_price.pkl','rb'))
 df = pd.read_csv('Laptop-data.csv')
 
-st.title("Laptop Predictor")
+st.title(":violet[LAPTOP PRICE PREDICTOR]")
+col1,col2,col3=st.columns(3)
+with col1:
+    st.subheader(':violet[COMAPANY]')
+    company = st.selectbox('Brand',df['Company'].unique())
 
-# brand
-company = st.selectbox('Brand',df['Company'].unique())
+    st.subheader(':violet[TYPE]')
+    type = st.selectbox('Type',df['TypeName'].unique())
 
-# type of laptop
-type = st.selectbox('Type',df['TypeName'].unique())
+    st.subheader(':violet[RAM]')
+    ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
 
-# Ram
-ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+    # weight
+    st.subheader(':violet[WEIGHT]')
+    weight = st.number_input('Weight of the Laptop')
+    
+    # Touchscreen
+    st.subheader(':violet[TOUCH SCREEN]')
+    touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+with col2:
+    # IPS
+    st.subheader(':violet[IPS]')
+    ips = st.selectbox('IPS',['No','Yes'])
 
-# weight
-weight = st.number_input('Weight of the Laptop')
+    # screen size
+    st.subheader(':violet[SCREEN SIZE]')
+    screen_size = st.number_input('Screen Size')
 
-# Touchscreen
-touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+    # resolution
+    st.subheader(':violet[RESOLUTION]')
+    resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
 
-# IPS
-ips = st.selectbox('IPS',['No','Yes'])
-
-# screen size
-screen_size = st.number_input('Screen Size')
-
-# resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
-
-#cpu
-cpu = st.selectbox('CPU',df['Cpu brand'].unique())
-
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
-
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
-
-gpu = st.selectbox('GPU',df['Gpu brand'].unique())
-
-os = st.selectbox('OS',df['os'].unique())
-
-if st.button('Predict Price'):
+    #cpu
+    st.subheader(':violet[CPU]')
+    cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+    
+    # hdd
+    st.subheader(':violet[HDD]')
+    hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+with col3:
+    # ssd
+    st.subheader(':violet[SSD]')
+    ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+    
+    # gpu
+    st.subheader(':violet[GPU]')
+    gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+    
+    # os
+    st.subheader(':violet[OS]')
+    os = st.selectbox('OS',df['os'].unique())
+    
+    submit=st.button('Predict Price')
+if submit:
     # query
     ppi = None
     if touchscreen == 'Yes':
